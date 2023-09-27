9b3bb38ce20913a7ccdac90eda4553d1a5792b10
Guilherme-Vianna <mataveli91@gmail.com>
2023-09-27 11:26:30 -0300

    project started

---
+++ src/main/java/com/soluctiontree/lgpd/helper/LgpdHelper.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.soluctiontree.lgpd.helper;
+
+/**
+ *
+ * @author User
+ */
+public class LgpdHelper {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
